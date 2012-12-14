fd0d2479611d3c919d0d5ee2cc4510b0c84eb70b
Justin Kramer <jkkramer@gmail.com>
2012-12-14 14:50:32 -0500

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jkkramer/purty "0.1.0-SNAPSHOT"
+(defproject jkkramer/purty "0.1.0"
   :description "Perform basic image operations - read, write, scale, crop"
   :url "http://github.com/jkk/purty"
   :license {:name "Eclipse Public License"
